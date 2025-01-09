457429cfb3d6e735c83671425ece4c2031cbeed2
emilkjellin <emil.kjellin@elev.ga.ntig.se>
2025-01-09 10:13:24 +0100

    Gjort fler uppgifter i uppgift 3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void hello() {
 
@@ -5,9 +8,45 @@ public class Main {
             System.out.println("Hello world!");
         }
     }
-            public static void main(String[] args)
-        {
-         hello();
+            public static void square(int a) {
+                System.out.println(a*a);
+            }
+            public static void multi(float a, float b)
+            {
+                System.out.println(a*b);
+            }
+            public static void RightTriangleArea(float a, float b)
+            {
+                System.out.println((a*b)/2);
+            }
+            public static void CircleArea(float a)
+            {
+                 System.out.println((a*a)*3.14);
+            }
 
-    }
+            public static void Getnumberinput()
+            {
+                Scanner intinput = new Scanner(System.in);
+                while (true){
+                 try {
+                     System.out.println("Enter a number");
+                     int number = intinput.nextInt();
+                     System.out.println("you picked the number" + number);
+                     break;
+                 } catch (InputMismatchException e) {
+                     intinput.nextLine();
+                     System.out.println("please enter a number");
+                 }
+                }
+            }
+            
+
+            public static void main(String[] args) {
+                hello();
+                 square(4);
+                multi(4,6);
+                RightTriangleArea(4,6);
+                CircleArea(4);
+                Getnumberinput();
+            }
 }
